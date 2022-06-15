5d0fa1fbb7b14cee01320e8e72731510bf6e74f1
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2022-06-15 08:24:40 +0700

    mengerjakan parser

--- Low Level Calculator/src/calculator.c
+++ Low Level Calculator/src/calculator.c
@@ -5,8 +5,13 @@
 #define false 0
 #define BUFFER_SIZE 256
 #define MAX_INT 0xffffffff
+#define NIL '\0'
+
+//enum buat ngecek operan mana yang sedang dicek
+enum posisi{KIRI,KANAN};
 
 int solve(char* buffer);//menyelesaikan soal
+int compute(int op1,char operator,int op2);//evaluasi operasi
 bool Equal(int num1,int num2);//mengembalikan true apabila num1==num2, selain itu false
 bool NotEqual(int num1,int num2);//mengembalikan true apabila num1!=num2, selain itu false
 bool Not(int num);//logical not -> !0xf==0, !0x0==1,!0xd==0
@@ -14,54 +19,119 @@ bool Greater(int num1,int num2);//num1>num2 == true, selain itu false
 bool GreaterEqual(int num1,int num2);//num1 >=num2 == true, selain itu false
 bool Lesser(int num1,int num2);//num1<num2==true, selain itu false
 bool LesserEqual(int num1,int num2);//num1 <=num2 == true, selain itu false
-int add(int num1,int num2);//num1+num2
+int tambah(int num1,int num2);//num1+num2
+int CharToInteger(char c);
+
+//TODO
+int kali(int num1,int num2);//num1*num2
 
 int main(){
     char buffer[BUFFER_SIZE];
     char response;
 
     while(true){
+        printf("Masukkan persamaan yang ingin diselesaikan. Pisahkan antar operand dan operator dengan spasi\n");
+        printf("Contoh: \"200 + 30 / -300\"\n");
+        printf("Operator yang valid: +,-,*,/,^");
         fgets(buffer,BUFFER_SIZE,stdin);
         printf("Hasil Operasi: %d\n",solve(buffer));
-        printf("Apakah Anda ingin melanjutkan perhitungan?(Y/N)");
+        printf("Apakah Anda ingin melanjutkan perhitungan?(Y/N)\n");
         response = getchar();
         if(Equal(response,'N')|Equal(response,'n')){
-            break;
+            goto END;
         }
         else{
             while  (NotEqual(response = getchar(),'\n') & NotEqual(response,EOF) ) { }
         }
     }
+    END:
     return 0;
 }
 int solve(char* buffer){
-    int result;
-    int op1=0,op2=0;
+    int result=0;
+    int operandKanan = 0;
     int i =0;
-    char operator='\0';
-    //operator yang valid: +(add),-(substract),*(multiply),/(integer division),^(pangkat)
+    char operator=NIL;
+    enum posisi curPos = KIRI;
+    bool isNegatif = false;
+    bool isParseStarted = false;//menandakan apakah token pertama sudah mulai dibaca
+    bool isNeedProcessed = false;//menandakan apakah perlu dilakukan operasi
+  //  bool op1Filled=false,op2Filled=false;//menandakan apakah operand kiri / operand kanan sudah terisi
+    //operator yang valid: +(tambah),-(substract),*(multiply),/(integer division),^(pangkat)
     //parse operasinya
     while(NotEqual(buffer[i],0x0)){//selama isi buffernya gak \0
         //skip spasi
         while(Equal(buffer[i],' ')){
-            i++;
+       //     if(NotEqual(operator,NIL) & op1Filled & op2Filled){
+            if(isNegatif){
+                if(Equal(curPos,KANAN)){
+                    operandKanan = kali(operandKanan,-1);
+                }
+                else{
+                    result = kali(result,-1);
+                }
+                isNegatif = false;
+            }
+            if(Equal(curPos,KIRI) & isParseStarted){
+                //LHS sudah dibaca
+                curPos = KANAN;
+            }
+            if(isNeedProcessed){
+                //proses operasi
+                result = tambah(result,compute(result,operator,operandKanan));
+                isNeedProcessed = false;
+                operator = NIL;
+            }
+            i = tambah(i,1);
         }
+        isParseStarted = true;
         //berupa angka 0-9
-        if(buffer[i]>=48 && buffer[1]<=57){
-            if(operator==NULL){
-                op1= (op1*10)+buffer[i];//operand kiri
+        if(GreaterEqual(buffer[i],48) & LesserEqual(buffer[1],57)){
+            if(Equal(curPos,KIRI)){
+                result= tambah(kali(result,10),CharToInteger(buffer[i]));//operand kiri
             }
             else{
-                op2 = (10*op2)+buffer[i];//operand kanan
+                isNeedProcessed = true;
+                operandKanan= tambah(kali(operandKanan,10),CharToInteger(buffer[i]));//operand kanan
             }
         }
-        //berupa operator tambah
-        else if(buffer[i]=='+'||buffer[i]=='-'){
-            operator='+';
+        //ketemu operator - (bisa negatif atau operator pengurangan)
+        else if(Equal(buffer[i],'-')){
+            if(NotEqual(buffer[i+1],' ')){
+                //kalau karakter sesudahnya bukan spasi, maka ini tanda negatif
+                //tandai buat dikasih negatif di akhir
+                isNegatif = true;
+            }
+            else{
+                //operator pengurangan
+                operator= buffer[i];
+            }
         }
-        i++;
+        //ketemu operator
+        else if(Equal(buffer[i],'+') | Equal(buffer[i],'*') | Equal(buffer[i],'/') | Equal(buffer[i],'^')){
+            //if(NotEqual(operator,NIL)){
+                //kalau operator!=NIL, maka ada operasi yang harus berjalan
+            //    result = tambah(result,compute(op1,operator,op2));
+                //reset operasi
+            //    operator = NIL;
+            //    op1 = result;
+            //    op2 = 0;
+           // }
+            operator= buffer[i];
+        }
+        i = tambah(i,1);
+    }
+    if(isNeedProcessed){
+        //periksa operasi terakhir
+        result = tambah(result,compute(result,operator,operandKanan));
     }
     return result;
+}
+int compute(int op1,char operator,int op2){
+    if(Equal(operator,'+')){
+        //penjumlahan
+        return tambah(op1,op2);
+    }
 }
 bool Equal(int num1,int num2){
     if(num1^num2){
@@ -74,7 +144,7 @@ bool NotEqual(int num1,int num2){
     return Not(Equal(num1,num2));//num1!=num2
 }
 bool Not(int num){
-    return add((num>>31)|((~num+1)>>31),1);
+    return tambah((num>>31)|((~num+1)>>31),1);
     //sama aja dengan : (num>>31 | -num>>31)+1
     // OR antara signed bit + dan - bilangan tersebut, kalau salah satunya angka 1, maka hasilnya adalah 0xff.ff yang kalau 
     //ditambah 1 pasti hasilnya 0. Satu satunya cara biar hasilnya gak 0 yakni bila hasil OR nya bukan 0xfffff yang 
@@ -85,7 +155,7 @@ bool Not(int num){
 }
 bool Greater(int num1,int num2){
     //return true bila num1>num2 (signed)
-    int difference = ~(add(~num2,num1));//selisih num1 dan num2
+    int difference = ~(tambah(~num2,num1));//selisih num1 dan num2
     return ((difference ^ ((num2 ^ num1) & (difference ^ num2)))>>31) & 0x1 ;
 }
 bool GreaterEqual(int num1,int num2){
@@ -99,12 +169,21 @@ bool GreaterEqual(int num1,int num2){
     //return true bila num1<num2 ATAU num1==num2
     return Lesser(num1,num2) | Equal(num1,num2);
 }
-int add(int num1,int num2){
-    int result = num1 ^ num2;
-    int carry = (num1 & num2)<<1;
+int tambah(int num1,int num2){
+    int result=0;
+    int carry = 0;
+loop:
+    result = tambah(result,num1 ^ num2);
+    carry = tambah(carry,(num1 & num2)<<1);
     if(isEqual(carry,0x0)){
         //rekursifnya kelar
         return result;
     }
-    return add(result,carry);
+    goto loop;
+}
+int kali(int num1,int num2){
+    return num1*num2;
+}
+int CharToInteger(char c){
+    return c-48;
 }
