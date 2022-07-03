02f8cdeeed165f7ef88b93bb645522c02154e916
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2022-07-03 17:41:31 +0700

    [Citer] belajar TCHAR, get process by name

--- Citer/src/memoryeditor.cpp
+++ Citer/src/memoryeditor.cpp
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <psapi.h>
+#include "atlstr.h"
 #pragma comment(lib, "psapi.lib")
+DWORD* getListOfProcessId(DWORD& processCount);
 void print_ps_name(DWORD pid) {
 	TCHAR pname[MAX_PATH] = TEXT("<unknown>");
 	HANDLE phandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
@@ -18,37 +20,42 @@ void print_ps_name(DWORD pid) {
 	CloseHandle(phandle);
 }
 void print_ps() {
-	DWORD ps_list[1024], ps_memctr, ps_ctr;
-	if (!EnumProcesses(ps_list, sizeof(ps_list), &ps_memctr))
-		return;
-	ps_ctr = ps_memctr / sizeof(DWORD);
+	DWORD ps_ctr = 0;
+	DWORD* ps_list = getListOfProcessId(ps_ctr);
 	for (int i = 0; i < ps_ctr; i++)
 		if (ps_list[i] != 0)
 			print_ps_name(ps_list[i]);
 }
-void print_ps__With_name(DWORD pid,std::string name) {
-	TCHAR pname[MAX_PATH] = TEXT("<unknown>");
-	TCHAR target[MAX_PATH] = TEXT(name);
-	HANDLE phandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
-	if (phandle != NULL) {
-		HMODULE pmod;
-		DWORD temp;
-		if (EnumProcessModules(phandle, &pmod, sizeof(pmod), &temp))
-			GetModuleBaseName(phandle, pmod, pname, sizeof(pname) / sizeof(TCHAR));
-	}
-	if (pname == name) {
-		_tprintf(TEXT("%s(PID %u)\n"), pname, pid);
-	}
-	CloseHandle(phandle);
-}
-void print_named_pid(std::string name) {
-	DWORD ps_list[1024], ps_memctr, ps_ctr;
+DWORD* getListOfProcessId(DWORD& processCount) {
+	DWORD ps_list[1024], ps_memctr;
 	if (!EnumProcesses(ps_list, sizeof(ps_list), &ps_memctr))
-		return;
-	ps_ctr = ps_memctr / sizeof(DWORD);
-	for (int i = 0; i < ps_ctr; i++)
-		if (ps_list[i] != 0)
-			print_ps__With_name(ps_list[i],name);
+		return NULL;
+	processCount = ps_memctr / sizeof(DWORD);
+	return ps_list;
+}
+HANDLE getProcess(TCHAR* name,DWORD& pid_) {
+	DWORD p_count = 0;
+	DWORD* processIdList = getListOfProcessId(p_count);
+	HANDLE phandle;
+	TCHAR pname[MAX_PATH] = TEXT("<unknown>");
+	if (processIdList != NULL) {
+		for (int i = 0; i < p_count; i++) {
+			if (processIdList[i] != 0) {
+				phandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processIdList[i]);
+				if (phandle != NULL) {
+					HMODULE pmod;
+					DWORD temp;
+					if (EnumProcessModules(phandle, &pmod, sizeof(pmod), &temp))
+						GetModuleBaseName(phandle, pmod, pname, sizeof(pname) / sizeof(TCHAR));
+					if (_tcscmp(name, pname) == 0) {
+						pid_ = processIdList[i];
+						return phandle;
+					}
+				}
+			}
+		}
+	}
+	return NULL;
 }
 int main() {
 	//print_ps();
@@ -56,8 +63,25 @@ int main() {
 	std::cout <<std::endl<< "Masukkan proses target:" << std::endl;
 	std::string name;
 	getline(std::cin, name);
-	std::cout << "Nama Aplikasi: "<<name;
-	print_named_pid(name);
-
+	char* procName = new char[name.length()+1];
+	strncpy(procName, name.c_str(), name.length() + 1);
+	procName[name.length()] = '\0';
+	std::cout << "Nama Proses: "<<procName<<'\n';
+	//konversi ke TCHAR
+	USES_CONVERSION;
+	TCHAR* namaApp = A2T(procName);
+	//_tprintf(TEXT("%s\n"), namaApp); -->buat debug
+	DWORD id;
+	HANDLE processHandler= getProcess(namaApp,id);
+	if (processHandler != NULL) {
+//		_tprintf(TEXT("%d\n"), id);
+		//cetak proses
+		_tprintf(TEXT("Proses %s ditemukan!! pid: %d\n"), namaApp, id);
+	}
+	else {
+		_tprintf(TEXT("Proses %s tidak ditemukan!\n"), namaApp, id);
+	}
+	//print_named_pid(name);
+	//print_ps();
 	return 0;
 }
